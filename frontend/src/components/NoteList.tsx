286731c4987de760db37dad0aeacd701e4c5be0c
Cinnamon Bun <nguyeney02@gmail.com>
2024-06-24 17:36:44 +0300

    Delete Toggable component and its test

--- frontend/src/components/NoteList.tsx
+++ frontend/src/components/NoteList.tsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
 import Note from "./Note";
 import NoteForm from "./NoteForm";
-import Togglable from "./Togglable";
 import { useNavigate } from "react-router-dom";
 import useAppContext from "../hooks/useAppContext";
 
 function NoteList() {
   const [showAll, setShowAll] = useState(true);
   const navigate = useNavigate();
-  const { notes, logout, user, noteFormRef } = useAppContext();
+  const { notes, logout, user } = useAppContext();
 
   const noteForm = () => {
-    return (
-      <Togglable buttonLabel="new note" ref={noteFormRef}>
-        <NoteForm />
-      </Togglable>
-    );
+    return <NoteForm />;
   };
 
   const handleLogout = () => {
