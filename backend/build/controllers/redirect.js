997b900c43b00809fd16a5d2581f3fcba414db11
chloee122 <130465629+chloee122@users.noreply.github.com>
2024-07-08 20:29:17 +0300

    remove build folder (#10)

--- backend/build/controllers/redirect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const redirectRouter = (0, express_1.Router)();
-redirectRouter.get("/", (_request, response) => {
-    response.redirect("/");
-});
-exports.default = redirectRouter;
--- backend/build/utils/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const MONGODB_URI = process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
-const PORT = process.env.PORT;
-exports.default = { MONGODB_URI, PORT };
