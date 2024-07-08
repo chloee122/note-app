103d6a1949a2e81c857f2505c68770f3c16b1fd1
Cinnamon Bun <nguyeney02@gmail.com>
2024-07-08 15:17:30 +0300

    refactor

--- backend/controllers/redirect.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const redirectRouter = Router();
-
-redirectRouter.get("/", (_request, response) => {
-  response.redirect("/");
-});
-
-export default redirectRouter;
