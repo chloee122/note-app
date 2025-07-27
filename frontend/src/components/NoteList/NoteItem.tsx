53c91e860c59c47170ed5966221db4888aa4f48b
Cinnamon Bun <nguyeney02@gmail.com>
2025-07-27 12:54:14 +0300

    Implement logic for formatting latest update time

--- frontend/src/components/NoteList/NoteItem.tsx
+++ frontend/src/components/NoteList/NoteItem.tsx
@@ -6,6 +6,7 @@ import {
   NoteItemText,
   NoteItemWrapper,
 } from "../styles/NoteItem.styled";
+import { showLatestUpdateTime } from "../../utils/timeUtils";
 
 interface NoteDetailsProps {
   note: Note;
@@ -25,7 +26,7 @@ function NoteItem({ note }: NoteDetailsProps) {
       <NoteItemContent>
         <NoteItemHeader>{note.title}</NoteItemHeader>
         <NoteItemText>{note.plainTextContent}</NoteItemText>
-        <span>Just now</span>
+        <span>{showLatestUpdateTime(note.updatedAt)}</span>
       </NoteItemContent>
     </NoteItemWrapper>
   );
---
+++ frontend/src/utils/timeUtils.ts
@@ -0,0 +1,24 @@
+const MS_PER_HOUR = 3600000;
+const MS_PER_SECOND = 1000;
+const HOURS_PER_DAY = 24;
+const SEC_PER_MINUTE = 60;
+const JUST_NOW_THRESHOLD = 3;
+
+export const showLatestUpdateTime = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const diffMs = Date.now() - date.getTime();
+  const hours = Math.floor(diffMs / MS_PER_HOUR);
+
+  if (hours > HOURS_PER_DAY)
+    return new Intl.DateTimeFormat("en", { dateStyle: "medium" }).format(date);
+
+  const seconds = Math.floor(diffMs / MS_PER_SECOND);
+  const minutes = Math.floor(seconds / SEC_PER_MINUTE);
+
+  if (hours >= 1) return hours === 1 ? "An hour ago" : `${hours} hours ago`;
+  if (minutes >= 1)
+    return minutes === 1 ? "A minute ago" : `${minutes} minutes ago`;
+  if (seconds < JUST_NOW_THRESHOLD) return `Just now`;
+
+  return `${seconds} seconds ago`;
+};
